7737524237fe904554d536cc75621e73f7f4b62d
Taheran Khan <pathantaheran1122@gmail.com>
2026-05-28 22:10:18 +0530

    fix build

--- src/utils/deferNonCritical.ts
+++ src/utils/deferNonCritical.ts
@@ -3,10 +3,10 @@ export function runWhenIdle(task: () => void, timeoutMs = 2000): () => void {
     task();
     return () => undefined;
   }
-  if ('requestIdleCallback' in window) {
+  if (typeof window.requestIdleCallback === 'function') {
     const id = window.requestIdleCallback(task, { timeout: timeoutMs });
     return () => window.cancelIdleCallback(id);
   }
-  const id = window.setTimeout(task, 1);
-  return () => window.clearTimeout(id);
+  const id = globalThis.setTimeout(task, 1);
+  return () => globalThis.clearTimeout(id);
 }
